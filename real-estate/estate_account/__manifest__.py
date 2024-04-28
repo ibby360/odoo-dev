27708b010ecad7acd847c9df0f8eef57b6dbc721
ibby360 <ibrahimramadhan360@outlook.com>
2024-04-28 23:15:58 +0300

    Initial commit

---
+++ real-estate/estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate Account',
+    'author':'ICTPack Solutions',
+    'sequence': -1,
+    'website': 'https://www.ictpack.tech',
+    'summary': 'realestate development',
+    'depends': ['estate', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo':[],
+    'installable': True,
+    'application': True,
+    'assets':{},
+    'license': 'LGPL-3',
+}
